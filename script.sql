60e1f99dcbd8f0a7b381386f5f5a4252bd40fa76
Sydney John <sjohn1@mines.edu>
2022-04-21 16:56:12 -0600

    initial commit

---
+++ script.sql
@@ -0,0 +1,4 @@
+CREATE TABLE neighborhood(neighborhood_number INT, neighborhood_name TEXT);
+
+CREATE TABLE commercial_property(reception_date DATE, sale_price INT, grantor TEXT, grantee TEXT, class VARCHAR(2), d_class TEXT, d_class_name TEXT);
+CREATE TABLE residential_property(reception_date DATE, sale_price INT, grantor TEXT, grantee TEXT, class VARCHAR(2), d_class TEXT, d_class_name TEXT);
